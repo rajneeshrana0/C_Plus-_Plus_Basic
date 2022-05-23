c11e9e6b0c118d9fcb6bef488f8b01099b56677c
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2022-05-23 21:11:12 +0530

    Add files via upload

---
+++ Code Studio C++/Find power of a number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, n;
+    cin >> x >> n;
+    int ans = 1;
+    while (n > 0)
+    {
+        ans *= x;
+        n--;
+    }
+    cout << ans;
+}
