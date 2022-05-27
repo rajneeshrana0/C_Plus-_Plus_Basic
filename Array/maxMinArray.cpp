4e22b2e83a59aac0b07165e0744a2ee3be6a79ca
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2022-05-27 13:45:08 +0530

    How to Find max or min in array

---
+++ Array/maxMinArray.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int getMin(int num[], int n)
+{
+    int min = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+
+        if (num[i] < min)
+        {
+            min = num[i];
+        }
+    }
+    // return minimum value
+    return min;
+}
+
+int getMax(int num[], int n)
+{
+    int max = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+
+        if (num[i] > max)
+        {
+            max = num[i];
+        }
+    }
+    // return max value
+    return max;
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of array : ";
+    cin >> size;
+
+    int num[100];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> num[i];
+    }
+    cout << "Maximum value is " << getMax(num, size) << endl;
+    cout << "Minimun value is " << getMin(num, size) << endl;
+    return 0;
+}
